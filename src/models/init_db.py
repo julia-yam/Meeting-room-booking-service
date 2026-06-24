141c395f14d0c04d0886a451a2e4ea89383277ca
julia-yam <amgurovaulia@gmail.com>
2026-06-24 21:29:17 +0500

    feat(models): complete database initialization script

---
+++ src/models/init_db.py
@@ -0,0 +1,12 @@
+import asyncio
+from src.models.base import Base
+from src.models.database import engine
+import src.models as models  
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        
+
+if __name__ == "__main__":
+    asyncio.run(create_tables())

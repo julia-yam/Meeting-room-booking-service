faebe7cfe5769db25deaae037f5c9e3b4b93ffa3
julia-yam <amgurovaulia@gmail.com>
2026-06-29 19:25:10 +0500

    style(rooms): fix Ruff E712 comparison to False using not operator

--- src/routers/rooms.py
+++ src/routers/rooms.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
-from src.models import Room, User
+from src.models import Room, User, Slot
 from src.models.database import get_async_session
 from src.routers.dependencies import get_current_user
 
@@ -12,6 +13,11 @@ router = APIRouter(prefix="/rooms", tags=["Rooms"])
 class RoomCreateSchema(BaseModel):
     name: str
     description: str | None = None
+
+class SlotCreateSchema(BaseModel):
+    room_id: int
+    start_time: datetime
+    end_time: datetime
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_room(
@@ -46,3 +52,42 @@ async def get_rooms(db: AsyncSession = Depends(get_async_session)):
     result = await db.execute(query)
     rooms = result.scalars().all()
     return rooms
+
+@router.post("/slots", status_code=status.HTTP_201_CREATED)
+async def create_slot(
+    slot_data: SlotCreateSchema,
+    db: AsyncSession = Depends(get_async_session),
+    current_user: User = Depends(get_current_user)
+):
+    room_query = select(Room).where(Room.id == slot_data.room_id)
+    room_result = await db.execute(room_query)
+    room = room_result.scalar_one_or_none()
+
+    if not room:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Указанная комната не найдена"
+        )
+
+    new_slot = Slot(
+        room_id=slot_data.room_id,
+        start_time=slot_data.start_time,
+        end_time=slot_data.end_time,
+        is_booked=False
+    )
+    db.add(new_slot)
+    await db.commit()
+    await db.refresh(new_slot)
+
+    return new_slot
+
+
+@router.get("/{room_id}/slots")
+async def get_available_slots(
+    room_id: int,
+    db: AsyncSession = Depends(get_async_session)
+):
+    query = select(Slot).where(Slot.room_id == room_id, not Slot.is_booked)
+    result = await db.execute(query)
+    slots = result.scalars().all()
+    return slots
